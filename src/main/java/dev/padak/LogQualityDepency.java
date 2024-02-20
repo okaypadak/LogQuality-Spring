d9ee28593e1a9b0054886e28f4ace6e6cce6dccd
kypdk@proton.me <kypdk@proton.me>
2024-02-20 14:26:44 +0300

    metrics çalıştı

--- src/main/java/dev/padak/LogQualityDepency.java
+++ src/main/java/dev/padak/LogQualityDepency.java
@@ -1,10 +1,24 @@
 package dev.padak;
 
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Objects;
 
 @Configuration
+@EnableScheduling
+@Slf4j
 public class LogQualityDepency {
 
 
@@ -25,5 +39,34 @@ public class LogQualityDepency {
 
     @Bean
     public LogQualityConfig logQualityConfig() { return new LogQualityConfig();}
+
+    private static final String METRIC_NAME = "system.cpu.usage";
+    private static final double MAX_USAGE = 0.50D;
+
+
+    @Autowired
+    MetricsEndpoint metricsEndpoint;
+
+    @Component
+    public class LogMetrics {
+
+        @Scheduled(fixedRate = 10000) // 10 seconds
+        public void logCPUUsage() {
+
+
+
+            Double systemCpuUsage = metricsEndpoint.metric(METRIC_NAME, null)
+                    .getMeasurements()
+                    .stream()
+                    .filter(Objects::nonNull)
+                    .findFirst()
+                    .map(MetricsEndpoint.Sample::getValue)
+                    .filter(Double::isFinite)
+                    .orElse(0.0D);
+
+            log.info("CPU {} ", systemCpuUsage);
+
+        }
+    }
 
 }
